77050d238ddf6203d9806545d53f9bfc014bf629
승희 <bangseunghee1108@gmail.com>
2023-10-29 22:25:17 +0900

    주석 추가 작성

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ class User {
     // 여기서 null은 '존재(자리)하지만 값이 없는' 상태를 의미한다.
     // 에러를 내지 않고 null을 대입해서 무분별한 에러를 방지한다.
 
+    // validateEmail 메서드 : 이메일 유효성 검사
     if(this.validateEmail(email)) {
       this._email = email;
     } else {
@@ -19,12 +20,12 @@ class User {
       // null 데이터타입의 값어치
     }
   }
+  
   // '가져오는', '읽어오는' 작업을 할 수 있는 getter 기능으로
   // 읽기 전용(readonly) 프로퍼티를 정의한다.
   // getter 함수는 키워드 get을 사용한다.
   // 실제 값인 _email을 새로 대입하거나 변경하는 목적이 아닌,
   // 읽어오는 목적으로만 사용한다.
-
   get eamil() {
     return this._email;
   }
@@ -40,11 +41,14 @@ class User {
   // setter 함수를 적절히 사용하기 위해 아래의 메서드를 제작했다.
   // return이 boolean으로, false 반환을 통해 constructor에서
   // null을 대입하는 것을 방지한다.
+
+  // 문자열 형식이 아니거나 
   validateEmail(email) {
     if(typeof email !== 'string') {
       return false;
     }
 
+    // "@"와, "."을 포함하지 않는 경우, 메서드는 'flase'를 반환하여 이메일이 유효하지 않음을 나타내기.
     if(!email.includes("@") || !email.includes(".")) {
       return false;
     }
